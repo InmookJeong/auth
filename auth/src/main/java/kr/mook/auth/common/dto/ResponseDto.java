f3bf730dd78939a6b61e4f485dbef60f48bc231c
InmookJeong <jeonginmook@gmail.com>
2025-09-28 22:14:16 +0900

    comment - ResponseDto 필드 주석 추가

--- auth/src/main/java/kr/mook/auth/common/dto/ResponseDto.java
+++ auth/src/main/java/kr/mook/auth/common/dto/ResponseDto.java
@@ -20,9 +20,18 @@ import lombok.NoArgsConstructor;
 @Data
 public class ResponseDto {
 	
+	// 결과 상태코드(ex. 200)
 	private String statusCode;
+	
+	// 결과 상태코드명(ex. HOME_ACCESS)
 	private String status;
+	
+	// 결과타입(ex. string)
 	private ResponseTypeEnum resultType;
+	
+	// 결과(ex. 접속을 환영합니다.)
 	private Object result;
+	
+	// 결과 언어(ex. ko-KR)
 	private LanguageEnum language;
 }
