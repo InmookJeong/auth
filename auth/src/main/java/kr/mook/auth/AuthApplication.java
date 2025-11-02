574b5bb3483bc89b16f3ada19813e980930b594b
InmookJeong <jeonginmook@gmail.com>
2025-11-02 16:49:06 +0900

    feat - Mapper 인터페이스 스캔을 위한 config 코드 추가

--- auth/src/main/java/kr/mook/auth/AuthApplication.java
+++ auth/src/main/java/kr/mook/auth/AuthApplication.java
@@ -1,9 +1,11 @@
 package kr.mook.auth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("kr.mook.auth.**.persistence")
 public class AuthApplication {
 
 	public static void main(String[] args) {
