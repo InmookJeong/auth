dbc20d47cd1ae05bc57f186c96015371c8cbdefb
InmookJeong <jeonginmook@gmail.com>
2025-11-10 22:10:11 +0900

    test - Home API 다국어 처리를 위한 테스트 코드 수정
    - API Header에 Accept-Language 추가
    - API 테스트 결과 검증 코드 수정

--- auth/src/test/java/kr/mook/auth/home/HomeControllerTest.java
+++ auth/src/test/java/kr/mook/auth/home/HomeControllerTest.java
@@ -1,6 +1,5 @@
 package kr.mook.auth.home;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
@@ -10,36 +9,37 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Locale;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import kr.mook.auth.common.dto.ResponseDto;
 import kr.mook.auth.common.enumeration.LanguageEnum;
 import kr.mook.auth.common.enumeration.ResponseTypeEnum;
+import kr.mook.auth.config.MessageSourceConfig;
 import kr.mook.auth.home.controller.HomeController;
-import kr.mook.auth.home.service.HomeService;
+import kr.mook.auth.home.service.impl.HomeServiceImpl;
 
 @AutoConfigureRestDocs(outputDir = "target/snippets")
 @WebMvcTest(HomeController.class)
 @ExtendWith(RestDocumentationExtension.class)
+@Import({HomeServiceImpl.class, MessageSourceConfig.class})
 public class HomeControllerTest {
 	
 	@Autowired
 	private MockMvc mockMvc;
-
-	@MockitoBean
-	private HomeService homeService;
+	
 	
 	@BeforeEach
 	void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
@@ -50,23 +50,16 @@ public class HomeControllerTest {
 	
 	@Test
 	void home() throws Exception {
-		ResponseDto successResponseDto = ResponseDto.builder()
-											 .status("200")
-											 .statusCode("HOME_ACCESS")
-											 .resultType(ResponseTypeEnum.STRING)
-											 .result("접속을 환영합니다.")
-											 .language(LanguageEnum.KOREAN)
-											 .build();
-		when(homeService.home()).thenReturn(successResponseDto);
 		
 		mockMvc.perform(get("/api/home")
-				.accept(MediaType.APPLICATION_JSON))
+				.accept(MediaType.APPLICATION_JSON)
+				.header("Accept-Language", Locale.KOREAN))
 				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.statusCode").exists())
-				.andExpect(jsonPath("$.status").exists())
-				.andExpect(jsonPath("$.resultType").exists())
-				.andExpect(jsonPath("$.result").exists())
-				.andExpect(jsonPath("$.language").exists())
+				.andExpect(jsonPath("$.statusCode").value("200"))
+				.andExpect(jsonPath("$.status").value("HOME_ACCESS"))
+				.andExpect(jsonPath("$.resultType").value(ResponseTypeEnum.STRING.name()))
+				.andExpect(jsonPath("$.result").value("접속을 환영합니다."))
+				.andExpect(jsonPath("$.language").value(LanguageEnum.KOREAN.name()))
 				.andDo(print())
 				.andDo(document(
 						"home-test",
