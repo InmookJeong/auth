027896b010b84dde4eec4c18f70efe2fa54d9a2a
InmookJeong <jeonginmook@gmail.com>
2026-02-11 22:41:30 +0900

    feat - RestfulApiHttpStatusUtil에 HTTP Status Code 500 추가

--- auth/src/main/java/kr/mook/auth/common/http/RestfulApiHttpStatusUtil.java
+++ auth/src/main/java/kr/mook/auth/common/http/RestfulApiHttpStatusUtil.java
@@ -19,10 +19,12 @@ public class RestfulApiHttpStatusUtil {
 	 * OK(HTTP STATUS CODE : 200)의 명칭
 	 */
 	public static String OK_NAME = HttpStatus.OK.name();
+	
 	/**
 	 * OK(HTTP STATUS CODE : 200)의 상태코드 값(문자열) 반환
 	 */
 	public static String OK_CODE_STRING = String.valueOf(HttpStatus.OK.value());
+	
 	/**
 	 * OK(HTTP STATUS CODE : 200)의 상태코드 값(숫자) 반환
 	 */
@@ -32,10 +34,12 @@ public class RestfulApiHttpStatusUtil {
 	 * CREATED(HTTP STATUS CODE : 201)의 명칭
 	 */
 	public static String CREATE_NAME = HttpStatus.CREATED.name();
+	
 	/**
 	 * CREATED(HTTP STATUS CODE : 201)의 상태코드 값(문자열) 반환
 	 */
 	public static String CREATE_CODE_STRING = String.valueOf(HttpStatus.CREATED.value());
+	
 	/**
 	 * CREATED(HTTP STATUS CODE : 201)의 상태코드 값(숫자) 반환
 	 */
@@ -49,10 +53,12 @@ public class RestfulApiHttpStatusUtil {
 	 * BAD REQUEST(HTTP STATUS CODE : 400)의 명칭
 	 */
 	public static String BAD_REQUEST_NAME = HttpStatus.BAD_REQUEST.name();
+	
 	/**
 	 * BAD REQUEST(HTTP STATUS CODE : 400)의 상태코드 값(문자열) 반환
 	 */
 	public static String BAD_REQUEST_CODE_STRING = String.valueOf(HttpStatus.BAD_REQUEST.value());
+	
 	/**
 	 * BAD REQUEST(HTTP STATUS CODE : 400)의 상태코드 값(숫자) 반환
 	 */
@@ -62,10 +68,12 @@ public class RestfulApiHttpStatusUtil {
 	 * FORBIDDEN(HTTP STATUS CODE : 403)의 명칭
 	 */
 	public static String FORBIDDEN_NAME = HttpStatus.FORBIDDEN.name();
+	
 	/**
 	 * FORBIDDEN(HTTP STATUS CODE : 403)의 상태코드 값(문자열) 반환
 	 */
 	public static String FORBIDDEN_CODE_STRING = String.valueOf(HttpStatus.FORBIDDEN.value());
+	
 	/**
 	 * FORBIDDEN(HTTP STATUS CODE : 403)의 상태코드 값(숫자) 반환
 	 */
@@ -75,12 +83,28 @@ public class RestfulApiHttpStatusUtil {
 	 * NOT FOUND(HTTP STATUS CODE : 404)의 명칭
 	 */
 	public static String NOT_FOUND_NAME = HttpStatus.NOT_FOUND.name();
+	
 	/**
 	 * NOT FOUND(HTTP STATUS CODE : 404)의 상태코드 값(문자열) 반환
 	 */
 	public static String NOT_FOUND_CODE_STRING = String.valueOf(HttpStatus.NOT_FOUND.value());
+	
 	/**
 	 * NOT FOUND(HTTP STATUS CODE : 404)의 상태코드 값(숫자) 반환
 	 */
 	public static int NOT_FOUND_CODE_NUMBER = HttpStatus.NOT_FOUND.value();
+	
+	// ######################################
+	// #####    5XX HTTP STATUS CODE    #####
+	// ######################################
+	
+	/**
+	 * NOT FOUND(HTTP STATUS CODE : 500)의 상태코드 값(문자열) 반환
+	 */
+	public static String INTERNAL_SERVER_ERROR_CODE_STRING = String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value());
+	
+	/**
+	 * NOT FOUND(HTTP STATUS CODE : 500)의 상태코드 값(숫자) 반환
+	 */
+	public static int INTERNAL_SERVER_ERROR_CODE_NUMBER = HttpStatus.INTERNAL_SERVER_ERROR.value();
 }
