07ee5d06b6566bf52c446e26fa7355bf0a2a9e9e
InmookJeong <jeonginmook@gmail.com>
2026-02-26 22:36:29 +0900

    test - 이용약관 정보 삭제 및 삭제 오류 테스트 코드 작성

---
+++ auth/src/test/java/kr/mook/auth/terms/remove/RemoveTermsTest.java
@@ -0,0 +1,306 @@
+package kr.mook.auth.terms.remove;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Locale;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import kr.mook.auth.common.enumeration.ResponseTypeEnum;
+import kr.mook.auth.terms.remove.persistence.RemoveTermsMapper;
+
+/**
+ * 이용약관 정보 삭제 테스트
+ * 
+ * @since 2026. 02. 25.
+ * @version 0.1
+ * @author Inmook, Jeong
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+public class RemoveTermsTest {
+	
+	/* 다국어 테스트를 위한 Locale 정보 */
+	private final Locale _LOCALE_KO_KR = Locale.KOREA;
+	private final Locale _LOCALE_EN_US = Locale.US;
+	private final String _ACCEPT_LANGUAGE_KO_KR = "ko-KR";
+	private final String _ACCEPT_LANGUAGE_EN_US = "en-US";
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private RemoveTermsMapper _removeTermsMapper;
+	
+	/**
+	 * 각 테스트 코드 수행 전 테이블 생성 및 테스트 데이터 저장
+	 */
+	@Sql(
+		statements = {
+			"CREATE TABLE TERMS (" +
+			"	TERMS_NO BIGINT NOT NULL," +
+			"	USE_YN TINYINT NOT NULL DEFAULT 1," + 
+			"	REQUIRE_YN TINYINT NOT NULL DEFAULT 1," +
+			"	ORDER_NO BIGINT NOT NULL DEFAULT 9999," +
+			"	TITLE VARCHAR(2000) NOT NULL," +
+			"	ONTENTS LONGTEXT NOT NULL," +
+			"	CREATE_ID BIGINT NOT NULL," +
+			"	CREATE_DATE DATETIME NOT NULL," +
+			"	UPDATE_ID BIGINT," +
+			"	UPDATE_DATE DATETIME," +
+			"	CONSTRAINT TERMS_PK PRIMARY KEY(TERMS_NO)" +
+			");",
+			"INSERT INTO TERMS (TERMS_NO,USE_YN, REQUIRE_YN, ORDER_NO, TITLE, CONTENTS, CREATE_ID, CREATE_DATE)" + 
+			"VALUES (1, 1, 1, 1, '테스트 - 사이트 이용 약관', '사이트에 대한 설명과 이용 규칙 및 규정, 광고, 서비스 오류 사항, 운영 정책 등에 대한 내용이 작성됩니다.', 0, NOW() );" + 
+			"INSERT INTO TERMS (TERMS_NO,USE_YN, REQUIRE_YN, ORDER_NO, TITLE, CONTENTS, CREATE_ID, CREATE_DATE)" + 
+			"VALUES (2, 1, 0, 2, '테스트 - 개인정보 수집 및 이용', '개인정보보호법에 따라 수집되는 개인 정보와 이용 목적을 안내하고, 동의 거부 시 불이익에 대한 내용을 안내합니다.', 0, NOW());"
+		},
+		executionPhase = ExecutionPhase.BEFORE_TEST_METHOD
+	)
+	@BeforeEach
+	void createTempTableAndData() {}
+	
+	@Sql(
+		statements = {
+			"CREATE TABLE TABLE_SEQUENCES (" +
+			"	name VARCHAR(100) COMMENT '시퀀스 이름'," +
+			"	currval BIGINT UNSIGNED COMMENT '현재 시퀀스 값'" + 
+			");" +
+			"INSERT INTO TABLE_SEQUENCES VALUES('terms_no', 2);" + 
+			"DELIMITER $$ " + 
+			"CREATE FUNCTION NEXTVAL (seq_name VARCHAR(50)) " +
+			"RETURNS BIGINT UNSIGNED " +
+			"MODIFIES SQL DATA " +
+			"DETERMINISTIC " +
+			"BEGIN " +
+			"    DECLARE RET BIGINT UNSIGNED; " +
+			"    UPDATE TABLE_SEQUENCES SET currval = currval + 1 WHERE name = seq_name; " +
+			"    SELECT currval INTO RET FROM TABLE_SEQUENCES WHERE name = seq_name; " +
+			"    RETURN RET; " +
+			"END " +
+			"$$ " + 
+			"DELIMITER;"
+		},
+		executionPhase = ExecutionPhase.BEFORE_TEST_METHOD
+	)
+	@BeforeEach
+	void createTempSequenceTableAndData() {}
+	
+	/**
+	 * 각 테스트 코드 수행 후 테이블 제거
+	 */
+	@Sql(
+		statements = {
+			"DROP TABLE TERMS;"
+		},
+		executionPhase = ExecutionPhase.AFTER_TEST_METHOD
+	)
+	@AfterEach
+	void dropTable() {}
+	
+	/**
+	 * 이용약관 번호 오류 테스트<br/>
+	 * - 적합하지 않은 이용약관 번호를 전달하였을 경우, 이용약관 번호가 올바르지 않다는 메시지가 출력되는지 테스트<br/>
+	 * - TermsN가 0 이하의 숫자인 경우 400 에러 발생<br/>
+	 * - 결과 메시지는 한글로 출력되도록 다국어 적용
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	void testRemoveByUnvaildTermsNoWithLocaleKoKr() throws Exception {
+		long termsNo = 0L;
+		String httpStatusCode = "400";
+		String statusCode = "ERR-TMS-DEL-001";
+		String status = "DELETE ERROR";
+		String resultMessage = "이용약관 번호가 올바르지 않습니다. 이용약관 번호는 1이상의 숫자를 입력해주세요.";
+		String apiDocsDir = "terms/remove/remove-by-unvaild-terms-no/ko";
+		ResultMatcher resultMatcher = status().isBadRequest();
+		
+		this._testRemoveResponse(termsNo, this._LOCALE_KO_KR, this._ACCEPT_LANGUAGE_KO_KR, httpStatusCode, statusCode, status, resultMessage, apiDocsDir, resultMatcher);
+	}
+	
+	/**
+	 * 이용약관 번호 오류 테스트<br/>
+	 * - 적합하지 않은 이용약관 번호를 전달하였을 경우, 이용약관 번호가 올바르지 않다는 메시지가 출력되는지 테스트<br/>
+	 * - TermsN가 0 이하의 숫자인 경우 400 에러 발생<br/>
+	 * - 결과 메시지는 영어로 출력되도록 다국어 적용
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	void testRemoveByUnvaildTermsNoWithLocaleEnUs() throws Exception {
+		long termsNo = 0L;
+		String httpStatusCode = "400";
+		String statusCode = "ERR-TMS-DEL-001";
+		String status = "DELETE ERROR";
+		String resultMessage = "The Terms of Use number is incorrect. Please enter a number greater than 1.";
+		String apiDocsDir = "terms/remove/remove-by-unvaild-terms-no/en";
+		ResultMatcher resultMatcher = status().isBadRequest();
+		
+		this._testRemoveResponse(termsNo, this._LOCALE_EN_US, this._ACCEPT_LANGUAGE_EN_US, httpStatusCode, statusCode, status, resultMessage, apiDocsDir, resultMatcher);
+	}
+	
+	/**
+	 * 삭제할 이용약관 정보를 찾을 수 없는 경우<br/>
+	 * - 이용약관 정보를 최종적으로 삭제하는 과정에서 데이터를 찾을 수 없는 경우, 이용약관 정보를 찾을 수 없다는 메시지가 출력되는지 테스트<br/>
+	 * - 삭제할 이용약관 정보를 찾을 수 없는 경우 404에러 발생<br/>
+	 * - 결과 메시지는 한글로 출력되도록 다국어 적용
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	void testRemoveNotFoundErrorWithLocaleKoKr() throws Exception {
+		long termsNo = 5L;
+		
+		// 이용약관 정보를 수정할 데이터가 없는 경우
+		given(this._removeTermsMapper.delete(termsNo)).willReturn(0);
+		
+		String httpStatusCode = "404";
+		String statusCode = "ERR-TMS-DEL-002";
+		String status = "DELETE ERROR";
+		String resultMessage = "삭제할 이용약관 정보가 없습니다. 이용약관 번호를 다시 확인해주세요.";
+		String apiDocsDir = "terms/remove/terms-not-found/ko";
+		ResultMatcher resultMatcher = status().isNotFound();
+		
+		this._testRemoveResponse(termsNo, this._LOCALE_KO_KR, this._ACCEPT_LANGUAGE_KO_KR, httpStatusCode, statusCode, status, resultMessage, apiDocsDir, resultMatcher);
+	}
+	
+	/**
+	 * 삭제할 이용약관 정보를 찾을 수 없는 경우<br/>
+	 * - 이용약관 정보를 최종적으로 삭제하는 과정에서 데이터를 찾을 수 없는 경우, 이용약관 정보를 찾을 수 없다는 메시지가 출력되는지 테스트<br/>
+	 * - 삭제할 이용약관 정보를 찾을 수 없는 경우 404에러 발생<br/>
+	 * - 결과 메시지는 영어로 출력되도록 다국어 적용
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	void testRemoveNotFoundErrorWithLocaleEnUs() throws Exception {
+		long termsNo = 5L;
+		
+		// 이용약관 정보를 수정할 데이터가 없는 경우
+		given(this._removeTermsMapper.delete(termsNo)).willReturn(0);
+		
+		String httpStatusCode = "404";
+		String statusCode = "ERR-TMS-DEL-002";
+		String status = "DELETE ERROR";
+		String resultMessage = "There are no Terms of Use information to delete. Please check the Terms of Use number again.";
+		String apiDocsDir = "terms/remove/terms-not-found/en";
+		ResultMatcher resultMatcher = status().isNotFound();
+		
+		this._testRemoveResponse(termsNo, this._LOCALE_EN_US, this._ACCEPT_LANGUAGE_EN_US, httpStatusCode, statusCode, status, resultMessage, apiDocsDir, resultMatcher);
+	}
+	
+	/**
+	 * 이용약관 정보 삭제에 성공하는 경우<br/>
+	 * - 이용약관 정보를 최종적으로 삭제하는지 테스트<br/>
+	 * - 정상적으로 삭제될 경우 저장 결과 메시지가 출력되는지 테스트<br/>
+	 * - 이용약관 정보가 삭제될 경우 200 상태 코드 발생<br/>
+	 * - 결과 메시지는 한글로 출력되도록 다국어 적용
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	void testRemoveWithLocaleKoKr() throws Exception {
+		long termsNo = 1L;
+		
+		// 이용약관 정보 삭제에 성공한 경우
+		given(this._removeTermsMapper.delete(termsNo)).willReturn(1);
+		
+		String httpStatusCode = "200";
+		String statusCode = "TMS-DEL-001";
+		String status = "DELETE";
+		String resultMessage = "이용약관 정보가 삭제되었습니다.";
+		String apiDocsDir = "terms/remove/success/ko";
+		ResultMatcher resultMatcher = status().isOk();
+		
+		this._testRemoveResponse(termsNo, this._LOCALE_KO_KR, this._ACCEPT_LANGUAGE_KO_KR, httpStatusCode, statusCode, status, resultMessage, apiDocsDir, resultMatcher);
+	}
+	
+
+	/**
+	 * 이용약관 정보 삭제에 성공하는 경우<br/>
+	 * - 이용약관 정보를 최종적으로 삭제하는지 테스트<br/>
+	 * - 정상적으로 삭제될 경우 저장 결과 메시지가 출력되는지 테스트<br/>
+	 * - 이용약관 정보가 삭제될 경우 200 상태 코드 발생<br/>
+	 * - 결과 메시지는 영어로 출력되도록 다국어 적용
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	void testRemoveWithLocaleEnUs() throws Exception {
+		long termsNo = 1L;
+		
+		// 이용약관 정보 삭제에 성공한 경우
+		given(this._removeTermsMapper.delete(termsNo)).willReturn(1);
+		
+		String httpStatusCode = "200";
+		String statusCode = "TMS-DEL-001";
+		String status = "DELETE";
+		String resultMessage = "The Terms of Use information has removed.";
+		String apiDocsDir = "terms/remove/success/en";
+		ResultMatcher resultMatcher = status().isOk();
+		
+		this._testRemoveResponse(termsNo, this._LOCALE_EN_US, this._ACCEPT_LANGUAGE_EN_US, httpStatusCode, statusCode, status, resultMessage, apiDocsDir, resultMatcher);
+	}
+	
+	/**
+	 * 이용약관 번호 조회 테스트<br/>
+	 * - 이용약관 정보 삭제 중 오류가 발생하였을 경우에 대한 테스트<br/>
+	 * - 이용약관 정보 삭제 성공에 대한 테스트<br/>
+	 * 
+	 * @param termsNo : 이용약관 번호
+	 * @param locale : 다국어
+	 * @param acceptLanguage : 다국어 정보(ex. ko-KR 또는 en-US)
+	 * @param statusCode : 처리 상태 코드(ex. 400, 404)
+	 * @param status : 처리 결과 상태 구문
+	 * @param resultMessage : 처리 결과 메시지
+	 * @param apiDocsDir : API 문서 경로
+	 * @param resultMatcher : 예상되는 HTTP 상태
+	 * @throws Exception
+	 */
+	private void _testRemoveResponse(Long termsNo, Locale locale, String acceptLanguage, String httpStatusCode, String statusCode, String status, String resultMessage, String apiDocsDir, ResultMatcher resultMatcher) throws Exception {
+		mockMvc.perform(delete("/api/terms/{termsNo}", termsNo)
+				.header("Accept-Language", acceptLanguage)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(resultMatcher)
+				.andExpect(jsonPath("$.httpStatusCode").value(httpStatusCode))
+				.andExpect(jsonPath("$.statusCode").value(statusCode))
+				.andExpect(jsonPath("$.status").value(status))
+				.andExpect(jsonPath("$.resultType").value(ResponseTypeEnum.STRING.name()))
+				.andExpect(jsonPath("$.result").value(resultMessage))
+				.andExpect(jsonPath("$.locale").value(locale.toString()))
+				.andDo(print())
+				.andDo(document(
+						apiDocsDir,
+						responseFields(
+								fieldWithPath("httpStatusCode").description("HTTP 응답 상태 코드"),
+								fieldWithPath("statusCode").description("결과 상태 코드"),
+								fieldWithPath("status").description("상태코드 명칭(설명)"),
+								fieldWithPath("resultType").description("결과 타입(ex. Number, String)"),
+								fieldWithPath("result").description("결과 메시지"),
+								fieldWithPath("locale").description("사용 언어")
+								)
+						));
+	}
+
+}
